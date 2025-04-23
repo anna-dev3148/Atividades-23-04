b7c5c2528c651cfdadf774b4f67399a4cc287089
Anna Letícia Silva <annaleticiabarbosadasilva@gmail.com>
2025-04-23 19:37:49 -0300

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void) { 
+
+    int notas [5];
+     notas [0] = 5;
+     notas [1] = 8;
+     notas [2] = 3;
+     notas [3] = 7;
+     notas [4] = 4;
+
+    printf("x [0] = %d; \n", notas [0]);
+    printf("x [1] = %d; \n", notas [1]);
+    printf("x [2] = %d; \n", notas [2]);
+    printf("x [3] = %d; \n", notas [3]);
+    printf("x [4] = %d; \n", notas [4]);
+
+    return 0;
+}
